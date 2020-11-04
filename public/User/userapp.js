b7bd61db6466050910284ffb2ad27163ba0e4746
Talha Siddiqui <talhasiddiqui975@gmail.com>
2020-11-05 02:43:46 +0500

    first

--- public/User/userapp.js
+++ public/User/userapp.js
@@ -9,6 +9,7 @@ function subscribe(e) {
             confirmButtonText: `Yes`,
             cancelButtonColor:'#971414',
             confirmButtonColor:'#ec7916',
+            customClass: 'swal-wide',
           }).then((result) => {
             if (result.isConfirmed) {
               Swal.fire('You have subscribed this Bus', '', 'success')
@@ -21,6 +22,7 @@ function subscribe(e) {
             icon: 'error',
             title: 'Insufficient Credit',
             text: 'Sorry, you have insufficent credit,please recharge ',
+            customClass: 'swal-wide',
           })  
           return false;
     }
@@ -36,21 +38,40 @@ function userpgpassSave() {
     //     Swal.fire({
     //         icon: 'error',
     //         text: 'You have entered incorrect old password! ',
+    //         customClass: 'swal-wide',
     //       })
     // return false  
     // }
+    if(newpass===""||confnewpass===""||oldpass===""){
+      Swal.fire({
+          icon: 'warning',
+          text: 'Fields cannnot be empty, Please fill all the fields',
+          customClass: 'swal-wide',
+        })  
+        return false;
+  }
+    if(newpass.length<minlength||confnewpass.length<minlength){
+      Swal.fire({
+          icon: 'error',
+          text: 'Password length should not be less than 8 charracter',
+          customClass: 'swal-wide',
+        })  
+        return false;
+  }
+    if(newpass.length<minlength||confnewpass.length<minlength){
+      Swal.fire({
+          icon: 'error',
+          text: 'Password length should not be less than 8 charracter',
+          customClass: 'swal-wide',
+        })  
+        return false;
+  }
     if(newpass!==confnewpass){
         Swal.fire({
             icon: 'error',
             text: 'Your new passwords does not match!',
+            customClass: 'swal-wide',
           }) 
-          return false;
-    }
-    if(newpass.length<minlength||confnewpass.length<minlength){
-        Swal.fire({
-            icon: 'error',
-            text: 'Password length should not be less than 8 charracter',
-          })  
           return false;
     }
 
@@ -58,6 +79,7 @@ function userpgpassSave() {
         Swal.fire({
             icon: 'success',
             text: 'Password has been changed!',
+            customClass: 'swal-wide',
           }) 
     }
     document.getElementById("useroldpass").value="";
--- public/app.js
+++ public/app.js
@@ -4,11 +4,20 @@ function loginbtn(){
     // var userid=document.getElementById("").value;
     // var useremail=document.getElementById("").value;    
     // var userpassword=document.getElementById("").value;
+    if(lgnemailid===""||lgnpassword===""){
+        Swal.fire({
+            icon: 'warning',
+            text: 'Fields cannnot be empty, Please fill all the fields',
+            customClass: 'swal-wide',
+          })  
+          return false;
+    }
 //     if (lgnemailid!==useremail){
 //     Swal.fire({
 //         icon: 'error',
 //         title:'Sorry',
 //         text: 'There is no account register against this email address',
+//         customClass: 'swal-wide',
 //       }) 
 //       return false;  
 // } 
@@ -17,6 +26,7 @@ function loginbtn(){
 //         icon: 'error',
 //         title:'Sorry',
 //         text: 'There is no account register against this passenger ID',
+//         customClass: 'swal-wide',
 //       }) 
 //       return false;  
 // } 
@@ -25,6 +35,7 @@ function loginbtn(){
 //     Swal.fire({
 //         icon: 'error',
 //         text: 'The password you entered is incorrect',
+//         customClass: 'swal-wide',
 //       }) 
 //       return false;
 // }
@@ -39,17 +50,27 @@ function forgetpassreset() {
 //     Swal.fire({
 //         icon: 'error',
 //         text: 'There is no account registered against this email. Kindly provide registerd e-mail or Signup',
+//          customClass: 'swal-wide',
 //       }) 
 //       document.getElementById("forgetrestemail").value="";
 //       return false;
 // }
+if (forgotemail===""){
+    Swal.fire({
+        icon: 'warning',
+        text: 'Please enter email',
+        customClass: 'swal-wide',
+      }) 
+      return false;  
+}
     if (forgotemail.match(mailformat)) {
         
         return true;}
     else{
         Swal.fire({
-            icon: 'error',
+            icon: 'warning',
             text: 'Please enter a valid email address!',
+            customClass: 'swal-wide',
           }) 
           document.getElementById("forgetrestemail").value="";
           return false;  
@@ -71,6 +92,7 @@ var minlength=8;
 //         icon: 'error',
 //         title:'Sorry',
 //         text: 'An account already exixst with this passaengert ID',
+//          customClass: 'swal-wide',
 //       }) 
 //       return false;  
 // } 
@@ -79,6 +101,7 @@ var minlength=8;
 //         icon: 'error',
 //         title:'Sorry',
 //         text: 'An account already exixst with this email address',
+//          customClass: 'swal-wide',
 //       }) 
 //       return false;  
 // }
@@ -86,13 +109,15 @@ if(signuppass.length<minlength||signuppassagain.length<minlength){
     Swal.fire({
         icon: 'error',
         text: 'Password length should not be less than 8 charracter',
+        customClass: 'swal-wide',
       })  
       return false;
 } 
 if(signuppass!==signuppassagain){
     Swal.fire({
         icon: 'error',
-        text: 'Your new passwords does not match!',
+        text: 'Your passwords does not match!',
+        customClass: 'swal-wide',
       }) 
       return false;
 }
